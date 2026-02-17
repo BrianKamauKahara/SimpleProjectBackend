c6886851ad52fd8b4c01e3500c2629cf20597f7b
BrianKamauKahara <167960457+BrianKamauKahara@users.noreply.github.com>
2026-02-17 12:44:35 +0300

    gedub

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
-const development = process.env.NODE_ENV === "development"    
+const allowedEnv = process.env.NODE_ENV === "development" || process.env.NODE_ENV === "production"   
 
 const allowedOrigins = require('./allowedOrigins')
     
 const corsOptions = {
     origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || (!origin && development)) { // Remove for Development
+        if (allowedOrigins.indexOf(origin) !== -1 || (!origin && allowedEnv) ) { // Remove for Development
             callback(null, true)
         } else {
             console.log(origin)
