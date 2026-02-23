ab1eece17422eb3022618a5059965b4c129ffdf3
BrianKamauKahara <167960457+BrianKamauKahara@users.noreply.github.com>
2026-02-23 13:25:27 +0300

    small typo on the allowed origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,5 +2,5 @@ module.exports = [
     'https://www.mysite.com',
     'http://127.0.0.1:5173',
     'https://localhost:5500',
-    'https://notes-react-app-seven.vercel.app/'
+    'https://notes-react-app-seven.vercel.app'
 ]
