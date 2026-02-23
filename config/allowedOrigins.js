0ba1620b52d88a503ec0afa52d35d8bed53a477e
BrianKamauKahara <167960457+BrianKamauKahara@users.noreply.github.com>
2026-02-23 13:23:18 +0300

    added frontend origin to allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 module.exports = [
     'https://www.mysite.com',
     'http://127.0.0.1:5173',
-    'https://localhost:5500'
+    'https://localhost:5500',
+    'https://notes-react-app-seven.vercel.app/'
 ]
